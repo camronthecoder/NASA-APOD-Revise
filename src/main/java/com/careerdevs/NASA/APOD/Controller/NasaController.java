1da4cd81ae101e50fb7fab3a275392383b37349e
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-08-03 15:14:12 -0400

    jhvj

--- src/main/java/com/careerdevs/NASA/APOD/Controller/NasaController.java
+++ src/main/java/com/careerdevs/NASA/APOD/Controller/NasaController.java
@@ -1,23 +1,46 @@
 package com.careerdevs.NASA.APOD.Controller;
 
 
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestTemplate;
+////https://apod.nasa.gov/apod/ap220802.html
+//@RestController
+//@RequestMapping("/nasa")
+
+//public class NasaController {
+
+//    private String myNasaKey = "B56HiyvCcnfXdtnIilPgqsJBgKw7VIaJKLzxHvNG";
+//    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
+//
+
+//    }
+//    @GetMapping("/")
+//    private String routeRoute (){
+//        return "Your requested root";
+//    }
+//
+//    }
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-//https://apod.nasa.gov/apod/ap220802.html
-@RestController
-@RequestMapping("/nasa")
 
-public class NasaController {
+    @RestController
+    public class NasaController {
+        private String myNasaKey = "B56HiyvCcnfXdtnIilPgqsJBgKw7VIaJKLzxHvNG";
+        private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
 
-    private String myNasaKey = "B56HiyvCcnfXdtnIilPgqsJBgKw7VIaJKLzxHvNG";
-    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
+        @GetMapping("/")
+        private String routeRoute (){
+            return "Your requested root";
+        }
 
-    @GetMapping("/apod")
-    public Object apodHandler (RestTemplate restTemplate) {
-        return restTemplate.getForObject(nasaApodEndpoint, Object.class);
-    }
-    
-}
+            @GetMapping("/apod")
+    public Object apodRoute (RestTemplate restTemplate) {
+            return restTemplate.getForObject(nasaApodEndpoint, Object.class);
+            }}
+//
+
+
